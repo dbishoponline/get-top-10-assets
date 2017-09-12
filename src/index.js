91a5120e71307e02337dbeae3f946947a5e757c0
Dylan Bishop <dbishoponline@gmail.com>
2017-09-11 21:34:53 -0700

    adds comments to index

--- src/index.js
+++ src/index.js
@@ -1,3 +1,30 @@
+// Write a script that, given a web server log file, returns the 10 most frequently 
+// requested objects and their cumulative bytes transferred. Only include GET requests 
+// with Successful (HTTP 2xx) responses. Resolve ties however you’d like.
+
+// Log format:
+// - request date, time, and time zone
+// - request line from the client
+// - HTTP status code returned to the client
+// - size (in bytes) of the returned object
+
+// Given this input data:
+// ```
+// [01/Aug/1995:00:54:59 -0400] "GET /images/opf-logo.gif HTTP/1.0" 200 32511
+// [01/Aug/1995:00:55:04 -0400] "GET /images/ksclogosmall.gif HTTP/1.0" 200 3635
+// [01/Aug/1995:00:55:06 -0400] "GET /images/ksclogosmall.gif HTTP/1.0" 403 298
+// [01/Aug/1995:00:55:09 -0400] "GET /images/ksclogosmall.gif HTTP/1.0" 200 3635
+// [01/Aug/1995:00:55:18 -0400] "GET /images/opf-logo.gif HTTP/1.0" 200 32511
+// [01/Aug/1995:00:56:52 -0400] "GET /images/ksclogosmall.gif HTTP/1.0" 200 3635```
+
+// The result should be:
+// ```
+// /images/ksclogosmall.gif 10905
+// /images/opf-logo.gif 65022
+// ```
+
+// (You may tweak the output format as you'd like)
+
 require("babel-register")
 require("babel-polyfill")
 
